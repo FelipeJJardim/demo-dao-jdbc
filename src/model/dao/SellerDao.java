7ff0571bafb26740265c9eafe3f8415796b86747
Felipe Jardim <felipeaugustojardimdejesus@gmail.com>
2023-09-20 09:28:50 -0300

    DepartmentDao and SellerDao interfaces

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,16 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Seller;
+
 public interface SellerDao {
+
+	void insert(Seller obj); //responsavel por inserir no banco de dados o objeto 
+	void update(Seller obj); // responsavel por fazer o updade mno bancop de dados
+	void deleteById(Integer id); // responsavel por deletar um objeto no banco de dados
+	Seller findId(Integer id); // responsavel por comparar o objeto no banco de dados, se existir retorna o objeto se nao existir retorna nulo
+	List<Seller> findAll(); //responsavel por retornar todos os departamentos
+		
 
 }
